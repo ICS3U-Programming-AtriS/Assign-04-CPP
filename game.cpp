e17c1337e787b0ba2fb5ea0842bf01cb6cb65ec4
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-05-04 02:10:10 +0000

    Added event class

--- game.cpp
+++ game.cpp
@@ -96,9 +96,10 @@ void Orange(std::string msg = "") {
     std::cout << msg;
 }
 
-// Function thats check whether an element is in a list or not
-// I was originally planning to use a loop that checks over every element inside the list
-// but std::find() is way easier [I searched up how to do this]
+// Function thats check whether an element is in a list or not.
+// I was originally planning to use a loop that checks over
+// every element inside the list.
+// but std::find() is way easier [I searched up how to do this].
 bool IsElementInList(std::string element, std::list<std::string> list) {
     // list.end() does not represent the last element of a list
     // it represents 1 after the last element of the list
@@ -107,7 +108,8 @@ bool IsElementInList(std::string element, std::list<std::string> list) {
 }
 
 // Function that gets the player's decision
-std::string GetDecision(std::string question, std::list<std::string> decisions) {
+std::string GetDecision(std::string question,
+    std::list<std::string> decisions) {
     while (true) {
         // Display question
         Cyan(question);
@@ -167,9 +169,97 @@ void DisplayTitle() {
 }
 
 // INITIALIZE STATS
-long int gold = 100;
-long int population = 0;
-long int power = 100;
+// I was originally using [long int] but cpplint didn't like that
+int64_t gold = 100;
+int64_t population = 0;
+int64_t power = 100;
+
+// Initalize a list to hold the flags for the user
+std::list<std::string> userFlags = {};
+
+// EVENT EVENT
+class Event {
+    // Every property is going to be public
+    // Wierd indentation is for cpplint
+ public:
+        // Function that checks whether an event is available or not
+        bool Available() {
+            // Check if the user has the prerequisite flags
+            for (std::string flag : inclusionFlags) {
+                if (!IsElementInList(flag, userFlags)) {
+                    return false;
+                }
+            }
+
+            // Check if the user does not have the blacklisted flags
+            for (std::string flag : exclusionFlags) {
+                if (IsElementInList(flag, userFlags)) {
+                    return false;
+                }
+            }
+
+            // Check if the user meets the minimum stat criteria
+            if (population < minPopulation) {
+                return false;
+            }
+            if (gold < minGold) {
+                return false;
+            }
+            if (power < minPower) {
+                return false;
+            }
+
+            // Check if the user meets the maximum stat criteria
+            if ((maxPopulation != -1) && (population > maxPopulation)) {
+                return false;
+            }
+            if ((maxGold != -1) && (gold > maxGold)) {
+                return false;
+            }
+            if ((maxPower != -1) && (power > maxPower)) {
+                return false;
+            }
+
+            // Once all checks are passed, return true
+            return true;
+        }
+
+        // Function that displays the synopsis of the event
+        void synopsis() {
+            std::cout << "This is an event!" << std::endl;
+        }
+
+        // The event question that is asked to the user after the synopsis
+        std::string question = "Do you understand? (yes/no): ";
+
+        // List of possible decisions for the player to make
+        std::list<std::string> decisions = {"YES", "NO"};
+
+        // Function that displays the aftermath based on decision
+        void Effect(std::string decision) {
+            std::cout << "You picked " << decision << std::endl;
+        }
+
+        // Flag Lists
+        std::list<std::string> inclusionFlags;  // List of prerequisite flags
+        std::list<std::string> exclusionFlags;  // List of blacklisted flags
+
+        // Minimums and Maximums
+        // -1 will represent infinity
+        // These values are used to check the availability of the event
+        int minPopulation = 0;
+        int maxPopulation = -1;
+        int minGold = 0;
+        int maxGold = -1;
+        int minPower = 0;
+        int maxPower = -1;
+
+        // Event Name
+        std::string name = "Event Name";
+
+        // Weight determines the likelihood of an event
+        int weight = 0;
+};
 
 int main() {
     // Code Goes Here
