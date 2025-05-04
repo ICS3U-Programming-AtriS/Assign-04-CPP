2654150809ddcb89f886763bd3e8ab8f9def2002
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-05-04 00:56:35 +0000

    Added color functions

---
+++ game.cpp
@@ -0,0 +1,95 @@
+// Copyright (c) 2025 Atri Sarker All rights reserved.
+// .
+// Created By: Atri S
+// Date: MAY 3, 2025
+// King of the Ants
+
+#include <iostream>
+#include <string>
+
+// Colors
+const char BLACK[] = "\033[0;30m";  // BLACK
+const char RED[] = "\033[0;31m";  // RED
+const char GREEN[] = "\033[0;32m";  // GREEN
+const char YELLOW[] = "\033[0;33m";  // YELLOW
+const char BLUE[] = "\033[0;34m";   // BLUE
+const char PURPLE[] = "\033[0;35m";  // PURPLE
+const char CYAN[] = "\033[0;36m";    // CYAN
+const char WHITE[] = "\033[0;37m";   // WHITE
+const char GREY[] = "\033[38;5;240m";  // GREY
+const char ORANGE[] = "\033[38;5;208m";  // ORANGE
+
+// COLOR PRINT FUNCTIONS
+void Black(std::string msg = "") {
+    // APPLY COLOR
+    std::cout << BLACK;
+    // PRINT MESSAGE
+    std::cout << msg;
+}
+
+void Red(std::string msg = "") {
+    // APPLY COLOR
+    std::cout << RED;
+    // PRINT MESSAGE
+    std::cout << msg;
+}
+
+void Green(std::string msg = "") {
+    // APPLY COLOR
+    std::cout << GREEN;
+    // PRINT MESSAGE
+    std::cout << msg;
+}
+
+void Yellow(std::string msg = "") {
+    // APPLY COLOR
+    std::cout << YELLOW;
+    // PRINT MESSAGE
+    std::cout << msg;
+}
+
+void Blue(std::string msg = "") {
+    // APPLY COLOR
+    std::cout << BLUE;
+    // PRINT MESSAGE
+    std::cout << msg;
+}
+
+void Purple(std::string msg = "") {
+    // APPLY COLOR
+    std::cout << PURPLE;
+    // PRINT MESSAGE
+    std::cout << msg;
+}
+
+void Cyan(std::string msg = "") {
+    // APPLY COLOR
+    std::cout << CYAN;
+    // PRINT MESSAGE
+    std::cout << msg;
+}
+
+void White(std::string msg = "") {
+    // APPLY COLOR
+    std::cout << WHITE;
+    // PRINT MESSAGE
+    std::cout << msg;
+}
+
+void Grey(std::string msg = "") {
+    // APPLY COLOR
+    std::cout << GREY;
+    // PRINT MESSAGE
+    std::cout << msg;
+}
+
+void Orange(std::string msg = "") {
+    // APPLY COLOR
+    std::cout << ORANGE;
+    // PRINT MESSAGE
+    std::cout << msg;
+}
+
+int main() {
+    // Code Goes Here
+}
